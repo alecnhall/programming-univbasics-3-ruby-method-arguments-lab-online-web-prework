821025719ce48b161448d67ba078580bef1302f5
Alec Hall <alecnhall@gmail.com>
2019-07-19 20:45:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,3 +9,12 @@ def introduction_with_language(name, language)
 end
 
 introduction_with_language("Alec", "Ruby")
+
+def introduction_with_language_optional(name, language)
+  if language == "Ruby"
+    puts ("Hi, my name is #{name} and I am learning to program in #{language}.")
+  else
+    puts ("Hi, my name is #{name} and I am learning to program.")
+end
+
+introduction_with_language_optional("Alec", "Ruby")
